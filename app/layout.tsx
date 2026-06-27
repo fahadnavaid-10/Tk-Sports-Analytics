aea753af3b0cb92fa65bd7d8529d68064e151f84
fahadnavaid-10 <fahadcsengineer@gmail.com>
2026-06-27 23:11:38 +0500

    fix: update description in metadata to remove win rate

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "TK Sports Analytics | Free Sports Predictions",
-    description: "+518 Units. 9% ROI. 55% Win Rate. Join free today.",
+    description: "+518 Units. 9% ROI. Join free today.",
   },
   robots: {
     index: true,
